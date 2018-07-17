8cd8ea0b965659cd6cde36dfb29d6ba30863369d
ed42311 <ed42311@gmail.com>
2018-07-16 18:22:17 -0600

    first commit

---
+++ src/components/Nerd.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-for="member in team" :key="member.id">
+      {{ `${member.name} is the ${member.title}` }}
+    </div>
+  </div>
+</template>
+
+<script>
+const team = [
+  {
+    id: 1,
+    name: 'johnny',
+    title: 'duder',
+  },
+  {
+    id: 2,
+    name: 'suzie',
+    title: 'master ninja',
+  },
+  {
+    id: 3,
+    name: 'bobbie',
+    title: 'medic',
+  },
+];
+export default {
+  name: 'Nerd',
+  data() {
+    return {
+      msg: 'Welcome Team!',
+      team: team,
+    };
+  },
+};
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
